16e91e55fa6f89e73efd88b838ba303ecc2ccd65
kdh <dongri.kang@sansumg.com>
2025-04-11 16:24:44 +0900

    [refactoring] 42p

--- Project4/baseball.h
+++ Project4/baseball.h
@@ -8,6 +8,11 @@ class Baseball
 public:
     void guess(const string &guessNumber)
     {
+        assertIllegalArgument(guessNumber);
+    }
+private:
+    void assertIllegalArgument(const string &guessNumber)
+    {
         if (guessNumber.length() != 3)
         {
             throw length_error("Must be three letters.");
@@ -21,11 +26,14 @@ public:
             }
         }
 
-        if (guessNumber[0] == guessNumber[1]
-            || guessNumber[0] == guessNumber[2]
-            || guessNumber[1] == guessNumber[2])
+        if (isDuplicatedNumber(guessNumber))
         {
             throw invalid_argument("Must not have the same number");
         }
+    }
+
+    bool isDuplicatedNumber(const string &guessNumber)
+    {
+        return (guessNumber[0] == guessNumber[1]) || (guessNumber[0] == guessNumber[2]) || (guessNumber[1] == guessNumber[2]);
     }
 };
